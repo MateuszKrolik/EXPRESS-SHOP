636ca285d917e1d6eecee08a5b931c7aef3ed1d3
Mateusz <mateuszkrolik87@gmail.com>
2023-12-28 17:28:07 +0100

    8th Commit
    Refactored the request handling logic to improve the execution flow.
    
    The server now returns immediately after registering the 'data' and 'end' event listeners for the '/message' POST request.
    
    This prevents the execution of any subsequent code until the request data is fully parsed and written to the 'message.txt' file.
    
    The response is now sent within the 'end' event listener, ensuring that it is not sent until the request handling is complete.

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
+      res.writeHead(302, { Location: "/" });
+      return res.end(); // return to exit the function
     });
-    res.writeHead(302, { Location: "/" });
-    return res.end(); // return to exit the function
   }
 
   //   console.log(req.url, req.method, req.headers);
