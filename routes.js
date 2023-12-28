20c8120382908fc0ad3185e40ef1f9771c13cbbb
Mateusz <mateuszkrolik87@gmail.com>
2023-12-28 21:56:59 +0100

    13th Commit
    Modified the 'start' script in package.json to use nodemon for running the app. This change enables automatic server restarts whenever a file change is detected, improving the development workflow. Tested the functionality by making a minor change in routes.js, which triggered a server restart as expected.

--- routes.js
+++ routes.js
@@ -12,6 +12,7 @@ const requestHandler = (req, res) => {
     res.write("</html>");
     return res.end(); // return to exit the function
   }
+
   if (url === "/message" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
