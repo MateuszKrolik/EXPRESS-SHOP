ab174198cc752acf48180818ff050605c2287d8e
Mateusz <mateuszkrolik87@gmail.com>
2023-12-29 17:10:13 +0100

    18th Commit
    Added route handling with Express.js middleware
    
    1. Removed the dummy middleware.
    2. Added a middleware that always runs for every request, logging a message and allowing the request to continue.
    3. Added a middleware that handles requests to the /add-product route, logging a message and sending a response.
    4. Added a middleware that handles requests to the / route, logging a message and sending a response.

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-// const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
   next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
   res.send("<h1>Hello from Express!</h1>");
 });
 
-// const server = http.createServer(app);
-// server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
 app.listen(3000); // Express.js internally calls http.createServer() and passes the Express app to it
