92f6277c170b6acfd9bbb5fc1116c9e6ebbfdca0
Mateusz <mateuszkrolik87@gmail.com>
2023-12-29 20:55:15 +0100

    26th Commit
    Added a custom 404 page
    
    1. Created a new '404.html' file in the 'views' directory.
    2. Added a basic HTML5 skeleton to the '404.html' file.
    3. Set the title of the page to 'Page Not Found'.
    4. Added an 'h1' tag with the text 'Page Not Found' to the body of the page.
    5. Updated the 404 middleware in 'app.js' to send the '404.html' file as a response.
    6. Used 'path.join' to construct the path to '404.html', using '__dirname' to get the path to the project folder.
    7. Noted that we don't need to go up a level in the path because 'app.js' is in the root folder.
    8. Confirmed that the '404.html' file is served and a 404 status code is sent when navigating to a route that doesn't exist.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,11 +10,11 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin",adminRoutes); // order matters when using use() method, but not when using get()
+app.use("/admin", adminRoutes); // order matters when using use() method, but not when using get()
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000); // Express.js internally calls http.createServer() and passes the Express app to it
