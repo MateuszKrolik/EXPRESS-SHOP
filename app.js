172ea2f35f363257fe28e01b77e37d25cef204c0
Mateusz <mateuszkrolik87@gmail.com>
2023-12-30 19:38:32 +0100

    33rd Commit
    Convert HTML templates to Pug and render dynamic content
    
    1. Converted 'add-product.html' and '404.html' to Pug templates.
    2. Created 'add-product.pug' and '404.pug' in the views folder.
    3. Updated 'admin.js' to render 'add-product.pug' instead of sending 'add-product.html'.
    4. Passed 'pageTitle' as a dynamic variable to 'add-product.pug'.
    5. Updated 'app.js' to render '404.pug' instead of sending '404.html'.
    6. Added necessary CSS imports to the new Pug templates.
    7. Tested and confirmed that the new Pug templates are rendering correctly.

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set("view engine", "pug");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -18,7 +18,7 @@ app.use("/admin", adminData.routes); // order matters when using use() method, b
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000); // Express.js internally calls http.createServer() and passes the Express app to it
