27f75c5db70035f379004edaaad8e1529c6b4e65
Mateusz <mateuszkrolik87@gmail.com>
2023-12-31 19:04:59 +0100

    45th Commit
    Title: Implement file-based storage for products
    
    Details:
    1. Imported the core 'fs' and 'path' modules to work with the file system and construct paths.
    2. In the 'save' method of the 'Product' class, created a path to a 'products.json' file in a new 'data' folder in the project root.
    3. Used 'fs.readFile' to read the existing products from the file. If the file does not exist (i.e., an error occurs), initialized an empty array for products.
    4. Pushed the new product (i.e., 'this') to the 'products' array.
    5. Used 'fs.writeFile' to write the 'products' array back to the file in JSON format. Logged any errors that occur during this operation.
    6. In the static 'fetchAll' method of the 'Product' class, read the products from the file and returned them. If an error occurs (i.e., the file does not exist), returned an empty array.
    7. Noted a bug where 'p' is not defined in 'fetchAll'. Copied the path construction code from 'save' to 'fetchAll' as a temporary fix.
    8. Encountered a 'TypeError: Cannot read property 'length' of undefined' error, to be fixed in the next commit.

--- models/product.js
+++ models/product.js
@@ -1,14 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
 module.exports = class Product {
   constructor(t) {
     this.title = t; // this refers to the object created based on this class
   }
-  save() {  
-    products.push(this); 
+  save() {
+    // products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      // arrow function ensures that this refers to the class
+      //   console.log(err);
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
   static fetchAll() {
+    const p = path.join(
+      // to avoid p is not defined error
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
     // static methods can be called directly on the class itself and not on instances of the class
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return []; //dont need else because return stops execution of the function
+      }
+      return JSON.parse(fileContent);
+    });
     return products;
   }
 };
