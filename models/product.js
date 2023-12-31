0e5d8686072f604c1569a0c5686e36192112c642
Mateusz <mateuszkrolik87@gmail.com>
2023-12-31 21:51:28 +0100

    46th Commit
    Refactor Product Model to Reuse File Storage Code
    
    - Created a helper function  to handle the file reading and path construction logic.
    - This function takes a callback  as an argument, which it calls with the products data when the file reading operation is complete.
    - Refactored the  method to use , passing the callback function it receives as an argument.
    - Refactored the  method to use , passing a new callback function that handles the product saving logic.
    - Moved the path construction code to a global constant  to make it available in both  and .
    - This commit improves code reuse and readability in the  model.

--- models/product.js
+++ models/product.js
@@ -1,6 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
+// global to avoid p is not defined error
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+// create helper to reuse file storage code
+const getProductsFromFile = (cb) => {
+  // callback to avoid  'TypeError: Cannot read property 'length' of undefined'
+  // static methods can be called directly on the class itself and not on instances of the class
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      // return []; //dont need else because return stops execution of the function
+      cb([]); // cb is a callback function
+    } else {
+      //   return JSON.parse(fileContent);
+      cb(JSON.parse(fileContent));
+    }
+  });
+  return products;
+};
+
 const products = [];
 
 module.exports = class Product {
@@ -8,43 +31,16 @@ module.exports = class Product {
     this.title = t; // this refers to the object created based on this class
   }
   save() {
-    // products.push(this);
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      // arrow function ensures that this refers to the class
-      //   console.log(err);
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
   }
+  // arrow function ensures that this refers to the class
   // callback refers to anonymous function passed as argument in fetchAll in controllers/products.js
   static fetchAll(cb) {
-    // callback to avoid  'TypeError: Cannot read property 'length' of undefined'
-    const p = path.join(
-      // to avoid p is not defined error
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    // static methods can be called directly on the class itself and not on instances of the class
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        // return []; //dont need else because return stops execution of the function
-        cb([]); // cb is a callback function
-      }
-      //   return JSON.parse(fileContent);
-      cb(JSON.parse(fileContent));
-    });
-    return products;
+    getProductsFromFile(cb);
   }
 };
