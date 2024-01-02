efcf32ab8c8a267df8d0ed9d3866a3146bd24177
Mateusz <mateuszkrolik87@gmail.com>
2024-01-02 18:00:46 +0100

    57th Commit
    Title: Implement find by ID functionality in product model and use it in shop controller
    
    Details:
    - Added a new static method 'findByID' in the product model that takes an ID and a callback as arguments.
    - The 'findByID' method reads all products from the file and uses the 'find' method to find the product with the matching ID.
    - In the shop controller, replaced the console log of the product ID with a call to 'Product.findByID'.
    - The product ID is passed to 'Product.findByID', and the resulting product is logged to the console.
    - Verified that the correct product details are logged when the 'Details' button is clicked for different products.
    
    Note: The functionality to display the product details page has not yet been implemented. This will be added in future commits.

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,7 +14,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
+  // console.log(prodId); //cant log asynch func
+  Product.findById(prodId, (product) => {
+    console.log(product);
+  });
   res.redirect("/");
 };
 
