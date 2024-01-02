a917dbcbf93072f33a3a7c01170174dfedefa893
Mateusz <mateuszkrolik87@gmail.com>
2024-01-02 17:04:35 +0100

    56th Commit
    Title: Add route to handle product details and extract product ID from URL
    
    Details:
    - Added a new route in 'shop.js' to handle requests to '/products/:productId'.
    - The ':productId' in the route is a dynamic segment that represents the ID of a specific product.
    - Created a new controller method 'getProduct' in 'shop.js' to handle requests to this route.
    - In 'getProduct', extracted the product ID from the request parameters using 'req.params.productId'.
    - Logged the product ID to the console and redirected the user to the homepage.
    
    Note: The functionality to display the product details page has not yet been implemented. This will be added in future commits.

--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,7 @@ const router = express.Router();
 router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
+router.get("/products/:productId", shopController.getProduct); //:productId is a dynamic segment
 router.get("/cart", shopController.getCart);
 router.get("/checkout", shopController.getCheckout);
 router.get("/orders", shopController.getOrders);
