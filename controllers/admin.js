e5749202db377b230c3ba512bb4b6ede04409dcf
Mateusz <mateuszkrolik87@gmail.com>
2024-01-03 21:19:38 +0100

    64th Commit
    Commit Title: Implement product deletion from product list and prepare for cart update
    
    Commit Description:
    1. Added a new static method  in the  model. This method is responsible for deleting a product by its ID.
    2. The  method works by first fetching all the products from the file using the  method.
    3. It then filters out the product with the provided ID from the product list, creating a new array of products without the deleted product.
    4. The updated product list is then written back to the file using .
    5. Prepared for the next commit where the deleted product will also be removed from the cart. This is indicated by the comment  at the end of the  method.
    6. This commit lays the groundwork for maintaining data consistency between the product list and the cart.

--- controllers/admin.js
+++ controllers/admin.js
@@ -74,3 +74,9 @@ exports.getAdminProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect("/admin/products");
+};
