14f4f3091ad507d8f16c803efb93b9ec4dceba41
Mateusz <mateuszkrolik87@gmail.com>
2024-01-04 18:27:16 +0100

    72nd Commit
    Title: Refactor Product Model and Controllers to Use SQL Database
    
    Body:
    - Removed testing code from app.js.
    - Removed file system and path dependencies from product.js as we are no longer working with files.
    - Removed helper function for getting products from a file in product.js.
    - Deleted code for saving, deleting, fetching, and finding by ID in product.js. These will be replaced with SQL queries.
    - Refactored fetchAll method in product.js to use SQL query to fetch all products from the database. The method now returns a promise instead of using a callback.
    - Imported pool object from database.js into product.js to execute SQL queries.
    - Updated getIndex controller in shop.js to handle the promise returned by Product.fetchAll. The controller now renders the 'shop/index' view once the promise is fulfilled.
    - Updated getProduct controller in shop.js to use Product.findById method which now uses SQL query to fetch a product by ID from the database.
    - Tested fetching of products from the database in the 'shop/index' view.

--- models/product.js
+++ models/product.js
@@ -1,92 +1,19 @@
-const fs = require("fs");
-const path = require("path");
+const db = require("../util/database");
 
 const Cart = require("./cart");
-
-// global to avoid p is not defined error
-const p = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "products.json"
-);
-
-// create helper to reuse file storage code
-const getProductsFromFile = (cb) => {
-  // callback to avoid  'TypeError: Cannot read property 'length' of undefined'
-  // static methods can be called directly on the class itself and not on instances of the class
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      // return []; //dont need else because return stops execution of the function
-      cb([]); // cb is a callback function
-    } else {
-      //   return JSON.parse(fileContent);
-      cb(JSON.parse(fileContent));
-    }
-  });
-  return products;
-};
-
-const products = [];
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
     this.id = id;
-    //names dont have to match
-    this.title = title; // this refers to the object created based on this class
+    this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
   }
-  save() {
-    getProductsFromFile((products) => {
-      //use also for updating an existing product
-      //check if already have na id
-      if (this.id) {
-        //update the product
-        const existingProductIndex = products.findIndex(
-          (prod) => prod.id === this.id
-        );
-        //replace product array with new product
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        //write to file
-        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random().toString();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-      }
-    });
+  save() {}
+  static deleteById(id) {}
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
   }
-  // arrow function ensures that this refers to the class
-  // callback refers to anonymous function passed as argument in fetchAll in controllers/products.js
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
-  static findById(id, cb) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id);
-      cb(product);
-    });
-  }
-  static deleteById(id) {
-    getProductsFromFile((products) => {
-      //extract product to get price
-      const product = products.find((prod) => prod.id === id);
-      const updatedProducts = products.filter((prod) => prod.id !== id);
-      if (updatedProducts) {
-        const updatedProducts = products.filter((prod) => prod.id !== id);
-        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-          if (!err) {
-            //remove from cart in next commit
-            Cart.deleteProduct(id, product.price);
-          }
-        });
-      }
-    });
-  }
+  static findById(id) {}
 };
