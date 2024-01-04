3994dcfef7c0832be40eb3e981c449b591cc16a5
Mateusz <mateuszkrolik87@gmail.com>
2024-01-04 21:13:35 +0100

    77th Commit
    Title: Define Product Model with Sequelize
    
    Body:
    - Deleted previous code in product.js and started from scratch.
    - Imported Sequelize and the database connection pool from database.js.
    - Defined a new Product model using sequelize.define.
    - Configured the Product model to have an 'id' field of type INTEGER, which auto-increments, does not allow null values, and is the primary key.
    - Configured the Product model to have a 'title' field of type STRING.
    - Configured the Product model to have a 'price' field of type DOUBLE, which does not allow null values.
    - Configured the Product model to have an 'imageUrl' field of type STRING, which does not allow null values.
    - Exported the Product model from product.js.
    
    This commit sets up the Product model to be managed by Sequelize, defining the structure of the 'products' table in the database.

--- models/product.js
+++ models/product.js
@@ -1,28 +1,30 @@
-const db = require("../util/database");
+const Sequelize = require("sequelize");
+//capitalized because it is a constructor function/class
+const sequelize = require("../util/database");
+//db connection pool
 
-const Cart = require("./cart");
+const Product = sequelize.define("product", {
+  //define model
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  //define fields
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+}); 
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-  save() {
-    return db.execute(
-      //returns a promise that execute yields
-      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-      //? is a placeholder to prevent SQL injection
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-  static deleteById(id) {}
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  }
-};
+module.exports = Product;
