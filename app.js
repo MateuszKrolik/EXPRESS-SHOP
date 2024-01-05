5ce25787e3e37d2275c695d8173d93163bec8a21
Mateusz <mateuszkrolik87@gmail.com>
2024-01-05 22:00:58 +0100

    87th Commit: Added User to Request Middleware. Removed the 'force: true' option from 'sequelize.sync()' to prevent overwriting tables. Added a check in the 'sequelize.sync().then()' block to find or create a User with id 1. Added a middleware function to the Express app that adds the User with id 1 to every request. This allows the User to be accessed in other parts of the application using 'req.user'.

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// middleware to add user to request
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      // store user in request
+      req.user = user;
+      next(); // continue with next middleware
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes); // order matters when using use() method, but not when using get()
 app.use(shopRoutes);
 
@@ -29,9 +40,20 @@ User.hasMany(Product);
 
 //tell sequelize to sync all models to db as tables whenever app starts
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Mateusz", email: "test@test.com" });
+    }
+    return user; // automatically wrapped in a promise
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
