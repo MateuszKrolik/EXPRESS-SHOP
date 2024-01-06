b7f4c3a5b96bc2277665fe068337b02cef335bef
Mateusz <mateuszkrolik87@gmail.com>
2024-01-06 19:37:37 +0100

    96th Commit
    Implement checkout functionality
    
    - Add 'Order Now' button in cart view.
    - Create new route and controller action for creating an order.
    - Implement logic to move cart items to a new order.
    - Redirect to orders page after successful order creation.

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
+//dont need to require Cart/Oder as i dont access it directly
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -137,6 +137,30 @@ exports.postCartDeleteProduct = (req, res, next) => {
   });
 };
 
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProducts(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => console.log(err));
+    })
+    .then((result) => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
+};
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     // .ejs can be omitted
