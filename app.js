a9e7f29c855ce0deab85254b0e8aceb838fd1cd8
Mateusz <mateuszkrolik87@gmail.com>
2024-01-06 19:04:21 +0100

    95th Commit
    Add order and order item models and relationships
    
    - Create order and order item models.
    - Define relationships between user, order, and product models.
    - Update database sync configuration to include new models.
    - Prepare for addition of checkout functionality.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -43,6 +45,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User); // optional
 Cart.belongsToMany(Product, { through: CartItem }); //telling sequelize where to find the join table
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem }); //telling sequelize where to find the join table
 
 //tell sequelize to sync all models to db as tables whenever app starts
 sequelize
