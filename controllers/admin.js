3c23625984671aafcfde144c6b50e05d32e54739
Mateusz <mateuszkrolik87@gmail.com>
2024-01-07 19:02:21 +0100

    106th Commit
    Title: Implement product editing functionality
    
    Details:
    1. Uncommented the  and  functions in the admin controller.
    2. Modified the  function to use the  method from the product model to fetch the product to be edited.
    3. Updated the  class in the product model to accept an optional fifth argument for the ID. If an ID is provided, it is stored in the  property of the product instance.
    4. Modified the  method in the  class to check if the  property is set. If it is, it updates the product in the database using the  method from MongoDB. If it's not, it inserts a new product using the  method.
    5. Updated the references to product ID in the edit-product.ejs file to use  instead of .
    6. Uncommented the get route for editing a product in the admin routes file.
    7. Tested the functionality by clicking on the edit button for a product and confirmed that the product details are displayed correctly in the form.
    
    Note: The MongoDB ObjectId is a special type that is used for IDs in MongoDB. When referencing IDs in the views, they need to be accessed using .

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,76 +30,73 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit === "true";
-//   if (!editMode) {
-//     return res.redirect("/");
-//   }
-//   //prepopulate the form with the product info
-//   const prodId = req.params.productId;
-//   req.user
-//     .getProducts({ where: { id: prodId } })
-//     // Product.findByPk(prodId)
-//     .then((products) => {
-//       //array of products
-//       const product = products[0];
-//       if (!product) {
-//         return res.redirect("/");
-//       }
-//       res.render("admin/edit-product", {
-//         pageTitle: "Edit Product",
-//         path: "/admin/edit-product",
-//         editing: editMode,
-//         product: product,
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-//   //   if (!product) {
-//   //     return res.redirect("/");
-//   //   }
-//   //   res.render("admin/edit-product", {
-//   //     pageTitle: "Edit Product",
-//   //     path: "/admin/edit-product",
-//   //     editing: editMode,
-//   //     product: product,
-//   //   });
-//   // });
-// };
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit === "true";
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  //prepopulate the form with the product info
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    // Product.findByPk(prodId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //   if (!product) {
+  //     return res.redirect("/");
+  //   }
+  //   res.render("admin/edit-product", {
+  //     pageTitle: "Edit Product",
+  //     path: "/admin/edit-product",
+  //     editing: editMode,
+  //     product: product,
+  //   });
+  // });
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//   //3 things: fetch info for product, create new product with updated info and call save
-//   const prodId = req.body.productId;
-//   const updatedTitle = req.body.title;
-//   const updatedImageUrl = req.body.imageUrl;
-//   const updatedPrice = req.body.price;
-//   const updatedDesc = req.body.description;
-//   Product.findByPk(prodId)
-//     .then((product) => {
-//       product.title = updatedTitle;
-//       product.price = updatedPrice;
-//       product.imageUrl = updatedImageUrl;
-//       product.description = updatedDesc;
-//       return product.save(); //return to not nest another promise
-//     })
-//     .then((result) => {
-//       //success promise
-//       console.log("UPDATED PRODUCT");
-//       res.redirect("/admin/products");
-//     })
-//     .catch((err) => console.log(err));
-//   //create new product
-//   // const updatedProduct = new Product(
-//   //   prodId,
-//   //   updatedTitle,
-//   //   updatedImageUrl,
-//   //   updatedDesc,
-//   //   updatedPrice
-//   // );
-//   // updatedProduct.save();
-//   // res.redirect("/admin/products");
-// };
+exports.postEditProduct = (req, res, next) => {
+  //3 things: fetch info for product, create new product with updated info and call save
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDesc = req.body.description;
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save(); //return to not nest another promise
+    })
+    .then((result) => {
+      //success promise
+      console.log("UPDATED PRODUCT");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+  //create new product
+  // const updatedProduct = new Product(
+  //   prodId,
+  //   updatedTitle,
+  //   updatedImageUrl,
+  //   updatedDesc,
+  //   updatedPrice
+  // );
+  // updatedProduct.save();
+  // res.redirect("/admin/products");
+};
 
 exports.getAdminProducts = (req, res, next) => {
   Product.fetchAll()
--- models/product.js
+++ models/product.js
@@ -2,19 +2,29 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb; //import by accessing getDb
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
   //save in db
   save() {
-    //manage one connection and return access to mongo
     const db = getDb();
-    return db //return to treat as promise
-      .collection("products")
-      .insertOne(this)
+    let dbOp; //db operation
+    if (this._id) {
+      //update product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });//update everything besides id
+    } else {
+      dbOp = db //return to treat as promise
+        .collection("products")
+        .insertOne(this);
+    }
+    //manage one connection and return access to mongo
+    return dbOp
       .then((result) => {
         console.log(this);
       })
