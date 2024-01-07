2baef5969869b75584b51794993f23d57513e1ba
Mateusz <mateuszkrolik87@gmail.com>
2024-01-07 22:56:18 +0100

    113th Commit
    Title: Implement Add to Cart Functionality in User Model
    
    Details:
    1. Modified the User model to include a `cart` field. This field stores the cart items for the user.
    2. Added a new method `addToCart` to the User model. This method accepts a product and adds it to the user's cart.
    3. In the `addToCart` method, created a new object `updatedCart` that includes the product and its quantity. Used the JavaScript spread operator to pull out all properties of the product and added a `quantity` field set to 1.
    4. Updated the user's cart in the database with the `updatedCart` object using the `updateOne` method on the `users` collection.
    5. Note: This commit sets up the basic add to cart functionality. It does not yet handle the case where the product already exists in the cart. This will be added in a future commit.

--- models/user.js
+++ models/user.js
@@ -4,15 +4,32 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
   // Methods
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
   }
+  //add to cart
+  addToCart(product) {
+    // const cartProducts = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] }; //spread operator to pull out all properties of product, then overwrite its quantity property
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      ); //overwrite cart with updatedCart
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
