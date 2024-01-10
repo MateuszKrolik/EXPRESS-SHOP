d65a90a9027d221bc94f2e0188485aebfdca72ad
Mateusz <mateuszkrolik87@gmail.com>
2024-01-10 18:11:59 +0100

    140th Commit
    Enhance cookie handling and security
    
    - Modified 'getLogin' and 'postLogin' controller actions in auth.js to handle cookies more securely.
    - In 'getLogin', commented out the extraction of 'loggedIn' value from the cookie. Now, 'isAuthenticated' is always set to false.
    - In 'postLogin', set the 'loggedIn' cookie with 'HttpOnly' flag. This prevents the cookie from being accessed through client-side JavaScript, providing protection against cross-site scripting (XSS) attacks.
    - Note: This commit does not yet implement sessions for storing sensitive information across requests. That will be covered in upcoming commits.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,14 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split("=")[1];
+  //   const isLoggedIn = req.get("Cookie").split("=")[1] === "true";
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   //   req.isLoggedIn = true;
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.setHeader("Set-Cookie", "loggedIn=true", "HttpOnly");
   res.redirect("/");
 };
