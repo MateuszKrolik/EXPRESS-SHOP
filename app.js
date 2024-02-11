5a88658092c826dfbce8d0f7630d67ce72f773b9
Mateusz <mateuszkrolik87@gmail.com>
2024-02-11 20:15:59 +0100

    187th Commit Title: Improved error handling and fixed infinite loop issue
    Details:
    - Added a dummy error in the middleware in app.js to simulate an error scenario.
    - Identified that throwing an error inside async code (promise, then, catch block) does not lead to the general error handling middleware being called.
    - Found that throwing an error outside of async code (synchronous code) does lead to the general error handling middleware being called.
    - Fixed an infinite loop issue caused by the middleware executing for every incoming request, by rendering the error page directly in the middleware instead of redirecting to it.
    - Moved the setting of the CSRF token before the user fetching middleware to avoid issues with token generation.
    - Noted that inside async code snippets, it's necessary to use 'next' wrapping the error, while in synchronous code, it's possible to just throw the error.

--- app.js
+++ app.js
@@ -42,6 +42,15 @@ app.use(csrfProtection); //after session
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals = {
+    isAuthenticated: req.session.isLoggedIn,
+    csrfToken: req.csrfToken(), //csrf token is added to every request
+  };
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
@@ -54,16 +63,8 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => {
-      throw new Error(err);
+      next(new Error(err));
     });
-});
-
-app.use((req, res, next) => {
-  res.locals = {
-    isAuthenticated: req.session.isLoggedIn,
-    csrfToken: req.csrfToken(), //csrf token is added to every request
-  };
-  next();
 });
 
 app.use('/admin', adminRoutes); // order matters when using use() method, but not when using get()
@@ -76,7 +77,12 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...);
-  res.redirect('/500');
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn,
+  });
 });
 
 mongoose
