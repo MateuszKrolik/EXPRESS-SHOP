649e247d246c9c35668b6614b175ed612d43a203
Mateusz <mateuszkrolik87@gmail.com>
2024-02-12 23:04:37 +0100

    211th Commit Title: Implement Stripe payment processing
    Details:
    - Reformatted data to match Stripe's requirements (currency, quantity, amount)
    - Added success and cancel URLs for Stripe redirection
    - Dynamically derived the URL and domain of the server
    - Forwarded the session ID to the view
    - Handled checkout success by gathering all products from the cart, creating an order, storing it in the database, and redirecting the user to orders
    - Handled checkout failure by redirecting the user back to the checkout page
    - Fixed Stripe error by adding a name
    - Tested checkout flow by adding a product to the cart, ordering it, and checking the orders page
    - Noted potential flaw with confirming order success

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 const fs = require('fs');
 const path = require('path');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
+const session = require('express-session');
 
 const ITEMS_PER_PAGE = 2;
 
@@ -137,20 +141,75 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
   req.user
     .populate('cart.items.productId')
     .then((user) => {
-      const products = user.cart.items;
-      let total = 0;
+      products = user.cart.items;
+      total = 0;
       products.forEach((p) => {
         total += p.quantity * p.productId.price;
       });
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items: products.map((p) => {
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+              unit_amount: p.productId.price * 100, //in cents
+            },
+            quantity: p.quantity,
+          };
+        }),
+        success_url:
+          req.protocol + '://' + req.get('host') + '/checkout/success',
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+      });
+    })
+    .then((session) => {
       res.render('shop/checkout', {
         path: '/checkout',
         pageTitle: 'Checkout',
         products: products,
         totalSum: total,
+        sessionId: session.id,
       });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      console.log(user.cart.items);
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, productData: { ...i.productId._doc } }; //._doc helps omit metadata, spread operator inside new object to pull out all data from document, then store it in productData
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email, //req.user is a full user object
+          userId: req.user, //moangoose will automatically pick the id
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart(); //clear cart after order is placed, clearCart is a method from user model
+    })
+    .then(() => {
+      res.redirect('/orders'); //execute only after clearCart is done
     })
     .catch((err) => {
       const error = new Error(err);
